92b0f90380b587f377398829eb866ae671e1d6d2
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-01-05 14:44:30 -0600

    added server routes

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+
+//USE
+app.use(express.json())
+app.use('/books', require('./controllers/books-controller'))
+
+//CONNECTING TO DB
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {console.log('connected to DB')})
+
+app.use(cors())
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+
+// Listen
+app.listen(process.env.PORT, () => {
+    console.log('Listening on', process.env.PORT)
+})
